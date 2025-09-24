965209fc905fbe670ee51ab53851c241b7c222f7
kranthi <donadikranthikumar@gmail.com>
2025-09-24 17:22:10 +0530

    main

---
+++ robohop.sh
@@ -0,0 +1,19 @@
+#!/bin/bah
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-069f722e1125dd734"
+
+for instance in $@
+do 
+INSTANCE_ID(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-069f722e1125dd734 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+#Get private IP
+if [ $instance !="frontend"]; then 
+IP=$(aws ec2 describe-instances --instance-ids i-068e3816986fd69a9 --query 'Reservations[0].Instances[0].PublicIpAddress' --output textaws ec2 describe-instances --instance-ids <YOUR_INSTANCE_ID> --query 'Reservations[0].Instances[*0].PublicIpAddress' --output textaws ec2 describe-instances --instance-ids <YOUR_INSTANCE_ID> --query 'Reservations[0].Instances[*0].PublicIpAddress' --output text)
+
+else 
+IP=$(aws ec2 describe-instances --instance-ids i-068e3816986fd69a9 --query 'Reservations[0].Instances[0].PublicIpAddress' --output textaws ec2 describe-instances --instance-ids <YOUR_INSTANCE_ID> --query 'Reservations[0].Instances[*0].PublicIpAddress' --output textaws ec2 describe-instances --instance-ids <YOUR_INSTANCE_ID> --query 'Reservations[0].Instances[*0].PublicIpAddress' --output text)
+
+fi
+echo "$instance: $IP"
+done

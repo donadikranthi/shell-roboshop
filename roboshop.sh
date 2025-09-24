0084f0aa989fd56478c9e055f015acedbf3b8c7f
kranthi <donadikranthikumar@gmail.com>
2025-09-24 15:47:32 +0530

    main

--- roboshop.sh
+++ roboshop.sh
@@ -8,8 +8,8 @@ do
 INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
 #Get private IP
-if [ $instance !="frontend"]; then 
-IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID--query 'Reservations[0].Instances[0].PublicIpAddress' --output textaws ec2 describe-instances --instance-ids <YOUR_INSTANCE_ID> --query 'Reservations[0].Instances[*0].PublicIpAddress' --output textaws ec2 describe-instances --instance-ids <YOUR_INSTANCE_ID> --query 'Reservations[0].Instances[*0].PublicIpAddress' --output text)
+if [ $instance != "frontend" ]; then 
+IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID--query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 
 else 
 IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output textaws ec2 describe-instances --instance-ids <YOUR_INSTANCE_ID> --query 'Reservations[0].Instances[*0].PublicIpAddress' --output textaws ec2 describe-instances --instance-ids <YOUR_INSTANCE_ID> --query 'Reservations[0].Instances[*0].PublicIpAddress' --output text)
